701b46c694605b7ca44d49409eaa68b8bf932d3a
Jikyung Kim <mr.j@Jikyungs-MBP.ht.home>
2024-03-26 11:20:25 -0400

    introduced frame rate function

--- ytwebp/frame_extractor.py
+++ ytwebp/frame_extractor.py
@@ -1,21 +1,27 @@
 import cv2
-from typing import Tuple, List
+from typing import List
 from numpy import ndarray
-from PIL.Image import Image
+from PIL.Image import Image 
+from PIL import Image as Img
 
-def extract_frames(video_link: str, output: str) -> List[Tuple[Image, int]]:
-    vidcap = cv2.VideoCapture("example.mp4")
+def extract_frames(video_link: str) -> List[Image]:
+    vidcap = cv2.VideoCapture(video_link)
     success, image = vidcap.read()
     frames = []
-    count = 0
     while success:
         image = _convert_cv2img_to_pilimg(image)
-        frames.append((image, count))
+        frames.append(image)
         success,image = vidcap.read() #extract frame from video
-        count += 1
     return frames
 
 
 def _convert_cv2img_to_pilimg(cv2_image: ndarray) -> Image:
     cv2_image  = cv2.cvtColor(cv2_image , cv2.COLOR_BGR2RGB) #convert cv2 color for PIL image
-    return Image.fromarray(cv2_image)
+    return Img.fromarray(cv2_image)
+
+def get_video_duration_seconds(video_link: str) -> float:
+    vidcap = cv2.VideoCapture(video_link)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    numOfFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+    durationInSeconds = numOfFrames//fps
+    return durationInSeconds
