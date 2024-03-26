f520bcbec395fa4e6b26e55baf8c3edbad38a441
Jikyung Kim <mr.j@Jikyungs-MBP.ht.home>
2024-03-26 11:20:02 -0400

    list of frames into webp

--- ytwebp/animator.py
+++ ytwebp/animator.py
@@ -1,7 +1,6 @@
-import numpy as np
-import cv2
 from PIL.Image import Image
 from typing import List, Tuple
 
-def animate_images_array(images_array: List[Tuple[Image, int]], output_file) -> None:
-    pass
+def animate_images_array(images_array: List[Tuple[Image, int]], video_duration, output_file) -> None:
+    images_array[0].save(f"{output_file}.webp", save_all = True, append_images = images_array[1:], optimize=False, duration=video_duration, loop=1)
+    return
