40a4ea33904325406ed107e1a5798e4829a6255f
Jikyung Kim <jay.jikyung.kim@mgmail.com>
2024-03-28 12:38:39 -0400

    removed tuple

--- ytwebp/animator.py
+++ ytwebp/animator.py
@@ -1,5 +1,5 @@
 from PIL.Image import Image
-from typing import List, Tuple
+from typing import List
 
 def animate_images_array(images_array: List[Image], output_file) -> None:
     images_array[0].save(f"{output_file}.webp", save_all = True, append_images = images_array[1:],duration= int(1000/30), optimize=False, loop=0)
