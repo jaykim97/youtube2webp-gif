d55dc0a8848b93bd4210bb358a19e1df34e44f72
Jikyung Kim <jay.jikyung.kim@mgmail.com>
2024-04-04 04:17:30 -0400

    allow setting start_time and duration

--- ytwebp/__main__.py
+++ ytwebp/__main__.py
@@ -7,14 +7,14 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Turn section of youtube video into animated webp")
-    parser.add_argument('url', help='URL of the YouTube video to process')
-    parser.add_argument('-s', '--start-time', help='Start time for the video (format: HH:MM:SS)')
+    parser.add_argument('url', type=str, help='URL of the YouTube video to process')
+    parser.add_argument('-s', '--start-time', type=str, help='Start time for the video (format: HH:MM:SS)')
     parser.add_argument('-d', '--duration', type=int, help='Duration of the video to process (in seconds)')
     args = parser.parse_args()
     
     url = utils.extract_video_url(args.url)
     print("video url extracted")
-    process = youtube_downloader.download_video(url)
+    process = youtube_downloader.download_video(url, start_time = args.start_time, duration = args.duration)
     print("video extraction complete")
     video_info = utils.extract_video_info(url)
     print("video info extration complete")
